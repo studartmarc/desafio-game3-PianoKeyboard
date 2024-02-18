b7876ae3001effb36a232f566b041c63afd9bae7
Marcelo Studart <studartmarc@gmail.com>
2024-02-18 14:00:03 -0300

    implementação dos sons em cada tecla específica com a integração da pasta tunes

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,8 +1,11 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
-const playTune = (key) => {
+let audio = new Audio("src/tunes/a.wav");
 
-};
+const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`;
+    audio.play();
+}
 
 pianoKeys.forEach((key) => {
     key.addEventListener("click", () => playTune(key.dataset.key))
